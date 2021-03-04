faa367fb8ae41c29b52b415530299ca7cbedc8bc
charleenmperrier <charlee_perrier@hotmail.com>
2021-03-04 03:24:04 +0000

    added js and sql

--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -9,14 +9,18 @@ const pool = new Pool({
 
 
 pool.query(`
-SELECT students.id as student_id, students.name as name, cohorts.name as cohort
-FROM students
+
+SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
+FROM teachers
+JOIN assistance_requests ON teachers.id = teacher_id
+JOIN students ON students.id = student_id
 JOIN cohorts ON cohorts.id = cohort_id
 WHERE cohorts.name LIKE '%${process.argv[2]}%'
-LIMIT ${process.argv[3] || 5};
+ORDER BY teacher;
+
 `)
 .then(res => {
   res.rows.forEach(user => {
-    console.log(`${user.name} has an id of ${user.student_id} and was in the ${user.cohort} cohort`);
+    console.log(`${user.cohort}: ${user.teacher}`);
   })
 }).catch(err => console.error('query error', err.stack));
